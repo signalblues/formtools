66e713ecd09f330e53e3794f14bcd42dc55a5a41
signalblues <signalblues@gmail.com>
2017-10-25 07:51:11 +0700

    Add New

---
+++ modules/form_builder/README.md
@@ -0,0 +1,12 @@
+## module-form_builder
+
+The Form Builder lets you create actual forms on your website. Check out the documentation for more information.
+
+### Documentation
+
+[http://modules.formtools.org/form_builder/](http://modules.formtools.org/form_builder/)
+
+### Form Tools Extensions
+
+For further information about Form Tools extensions, check out the following link:
+[http://www.formtools.org/extensions.php](http://www.formtools.org/extensions.php)
---
+++ modules/form_builder/library.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @copyright Benjamin Keen 2012
+ * @author Benjamin Keen <ben.keen@gmail.com>
+ */
+
+// -------------------------------------------------------------------------------------------------
+
+require_once(dirname(__FILE__) . "/global/code/forms.php");
+require_once(dirname(__FILE__) . "/global/code/hooks.php");
+require_once(dirname(__FILE__) . "/global/code/general.php");
+require_once(dirname(__FILE__) . "/global/code/generator.php");
+require_once(dirname(__FILE__) . "/global/code/module.php");
+require_once(dirname(__FILE__) . "/global/code/placeholders.php");
+require_once(dirname(__FILE__) . "/global/code/resources.php");
+require_once(dirname(__FILE__) . "/global/code/templates.php");
+require_once(dirname(__FILE__) . "/global/code/template_sets.php");
